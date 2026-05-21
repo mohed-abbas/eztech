b3f9944e8ae14d3043a0f965397a77249b86e2fe
Wilson San <wilson.san57@gmail.com>
2026-05-21 09:04:11 +0200

    connexion glitchtip web reussie

--- frontend/app/app.vue
+++ frontend/app/app.vue
@@ -6,3 +6,14 @@
     </NuxtLayout>
   </div>
 </template>
+
+<script setup>
+import * as Sentry from '@sentry/node'
+
+Sentry.init({
+  dsn: "https://65f35da24a7f484194ba95e404c41775@app.glitchtip.com/23595",
+  autoSessionTracking: false, // GlitchTip does not support sessions
+});
+
+Sentry.captureMessage("Test message from Node.js");
+</script>
